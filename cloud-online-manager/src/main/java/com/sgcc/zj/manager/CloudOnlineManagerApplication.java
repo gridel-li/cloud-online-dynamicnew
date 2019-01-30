d3ec3558960599d6985d4023200b11c03d2c6b6e
lyj <78606393@qq.com>
2019-01-30 17:28:38 +0800

    改成war包启动方式  jar也可以使用

--- cloud-online-manager/src/main/java/com/sgcc/zj/manager/CloudOnlineManagerApplication.java
+++ cloud-online-manager/src/main/java/com/sgcc/zj/manager/CloudOnlineManagerApplication.java
@@ -3,12 +3,13 @@ package com.sgcc.zj.manager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.sgcc.zj"})
 @MapperScan("com.sgcc.zj.common.dao")
 @EnableNeo4jRepositories("com.sgcc.zj.common.repository")
-public class CloudOnlineManagerApplication {
+public class CloudOnlineManagerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudOnlineManagerApplication.class, args);
